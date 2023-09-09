f8ec69d48cf903dce2110900dc5e6c8fe4bba085
YoanSamuel <yoansamuelfernandes@gmail.com>
2023-09-09 13:38:38 +0200

    fix database issue

---
+++ migrations/Version20230909113812.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230909113812 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE player ADD score NUMERIC(10, 0) NOT NULL');
+        $this->addSql('ALTER TABLE skull_king ADD first_player_id INT DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE skull_king DROP first_player_id');
+        $this->addSql('ALTER TABLE player DROP score');
+    }
+}
